c55a41b56c84af25ab242ab708a5823fe659d3f7
PgTothKornel <toth.kornel@diak.szbi-pg.hu>
2026-09-23 12:56:36 +0200

    alapok

---
+++ 01-create-components/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MainNavigation from './components/mainNavigation.vue';
+</script>
+
+<template>
+  <header>
+    <MainNavigation/>
+  </header>
+</template>
+
+<style scoped>
+</style>
---
+++ 01-create-components/src/components/MainNavigation.vue
@@ -0,0 +1,39 @@
+<!-- pascal case -->
+<script setup>
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li><a href="/">Vue app</a></li>
+            <li><a href="https://vuejs.org/">Vue app</a></li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+    nav {
+        background-color: #f0f0f0;
+        padding: 1rem;
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    li {
+        margin-bottom: 1rem;
+        margin: 1rem;
+    }
+
+    a {
+        text-decoration: none;
+        color: #333;
+        font-size: 1.2rem;
+    }
+
+
+</style>
